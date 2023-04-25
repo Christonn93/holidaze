5de96ef61a01da84c8f4b6b61c2d53dff079a578
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-04-25 10:34:11 +0200

    creating start fo component

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 // Importing MUI
-import { Box } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 
 // Importing hooks and functions
 import useApi from "../../hooks/useApi";
@@ -14,6 +14,8 @@ import VenueCard from "../../components/Cards/VenueCards/VenueCard";
 import ListingFilter from "../../components/Filter/ListingFilter";
 import UiFeedback from "../../components/UiFeedback/UiFeedback";
 import Loading from "../../components/Loading/Loading";
+
+import SearchIcon from "@mui/icons-material/Search";
 
 const Home = () => {
  updateHead("Home");
@@ -33,6 +35,21 @@ const Home = () => {
  return (
   <>
    <h1>Home</h1>
+   <Box
+    sx={{
+     display: "flex",
+     flexDirection: "column",
+     gap: 2,
+     marginTop: 5,
+     marginBottom: 5,
+    }}
+   >
+    <Typography>Testing new components</Typography>
+    <Box>
+     <TextField label={<SearchIcon />} id="search" placeholder="Search.." />
+    </Box>
+   </Box>
+
    <ListingFilter />
    {data.map((venue) => {
     return (
