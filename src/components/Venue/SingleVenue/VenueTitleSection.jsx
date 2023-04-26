e411b7b2b6ab074b7af771d12bbaa9a99b8bd1d8
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-04-26 09:55:18 +0200

    resoliving error

--- src/components/Venue/SingleVenue/VenueTitleSection.jsx
+++ src/components/Venue/SingleVenue/VenueTitleSection.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD:src/components/Venue/SingleVenue/VenueTitleSection.jsx
-import { changeTimeFormat } from "../../../js/changeTimeFormat";
-
-=======
->>>>>>> cc03e88a537014c3197beb85a117130ce57bbc70:src/components/Venue/VenueTitleSection.jsx
 import { Box, Typography } from "@mui/material";
 
 const VenueTitleSection = ({ name }) => {
