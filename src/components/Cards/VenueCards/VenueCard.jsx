740840eb586c3d7a9305b6ad38433263fbfd673a
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-05-05 15:46:58 +0200

    Fixing venue cards

--- src/components/Cards/VenueCards/VenueCard.jsx
+++ src/components/Cards/VenueCards/VenueCard.jsx
@@ -1,23 +1,30 @@
+// Importing React
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
+// Importing MUI
 import { Box, Button, Card, CardContent, CardMedia, Typography, Chip, useTheme, useMediaQuery } from "@mui/material";
 
+// Importing MUI Icons
 import WifiIcon from "@mui/icons-material/Wifi";
 import WifiOffIcon from "@mui/icons-material/WifiOff";
-
 import RestaurantIcon from "@mui/icons-material/Restaurant";
 import NoMealsIcon from "@mui/icons-material/NoMeals";
-
 import LocalParkingIcon from "@mui/icons-material/LocalParking";
-
 import GroupsIcon from "@mui/icons-material/Groups";
 
+// Importing style
 import { CardBase, CardBaseMobile, flexSpaceBetween } from "./VenueCardSx";
 
+/**
+ *
+ * @param {data} data
+ * @returns cards for listing
+ */
 const VenueCard = ({ data }) => {
  const device = useTheme();
  const isMobile = useMediaQuery(device.breakpoints.down("md"));
+ const navigate = useNavigate();
 
  // Destruction data
  const { name, maxGuests, price, media, meta, id, location } = data;
@@ -44,7 +51,7 @@ const VenueCard = ({ data }) => {
  const fixedName = rawName.charAt(0).toUpperCase() + rawName.slice(1);
 
  return (
-  <Card sx={!isMobile ? CardBase : CardBaseMobile}>
+  <Card sx={!isMobile ? CardBase : CardBaseMobile} onClick={() => navigate(`/venue/${id}`)}>
    <Box maxWidth={!isMobile ? 350 : 400} width={2 / 2}>
     <CardMedia src="image" component="img" image={media[0] ? media[0] : imageMissing} alt={media[0] ? "Main image of venue" : "Image is missing"} />
    </Box>
@@ -72,6 +79,7 @@ const VenueCard = ({ data }) => {
       }}
      >
       <Typography variant="h3">{fixedName}</Typography>
+
       <Typography variant="subtitle2">
        {address === "Unknown" ? <></> : <>{address},</>} {city === "Unknown" ? <></> : <>{city},</>} {country === "Unknown" ? <></> : <>{country}</>}
       </Typography>
@@ -93,6 +101,7 @@ const VenueCard = ({ data }) => {
       {breakfast ? <RestaurantIcon /> : <NoMealsIcon />}
       {parking ? <LocalParkingIcon /> : <></>}
      </Box>
+
      <Link to={`/venue/${id}`}>
       <Button variant="contained">Read more</Button>
      </Link>
